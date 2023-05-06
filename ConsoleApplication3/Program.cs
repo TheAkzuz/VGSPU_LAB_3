2ebef6c07c5f37ad6bea5671d22d10b1dc0511a9
TheAkzuz <131234047+TheAkzuz@users.noreply.github.com>
2023-05-06 10:48:39 +0300

    Add files via upload

---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x;
+            Console.WriteLine("Write X");
+            x = Console.ReadLine();
+            if (x.Length > 2)
+            {
+                int max = 0;
+                for (int i = 0; i < x.Length - 1; i++)
+                {
+                    int lo = int.Parse(Convert.ToString(x[i]));
+                    int lol = int.Parse(Convert.ToString(x[i + 1]));
+                    if (lo + lol > max)
+                    {
+                        max = lo + lol;
+                    }
+                }
+                Console.WriteLine("Max =" + max);
+            }
+            else
+            {
+                Console.WriteLine("X<3                ");
+            }
+
+
+        }
+    }
+}
